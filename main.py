31da6e39019308c10c79c0d2f9329a8c65d960c1
Ali Efe <65660565+A713F3@users.noreply.github.com>
2022-04-13 16:31:29 +0300

    update main.py

--- main.py
+++ main.py
@@ -6,6 +6,9 @@ load_dotenv('TOKEN.env')
 
 client = discord.Client()
 
+bot_command = '&'
+bot_commands = ['help', 'hello']
+
 def low(str):
     return str.lower()
 
@@ -18,21 +21,27 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content.startswith('>'):
+    if message.content.startswith(bot_command):
         commands = str(message.content)[1:]
         parsed = commands.split(" ")
 
         command = low(parsed[0])
         arguments = list(map(low, parsed[1:]))
 
+        if command not in bot_commands:
+            await message.channel.send(f"Invalid command, use {bot_command}help command for help.")
+            return 
+
         print(f"\nAUTHOR: {message.author}")
         print(f"COMMAND: {command} ")
         print(f"ARGUMENTS: {arguments}")
         
+        if command == "help":
+            await message.channel.send(f'Command List: {bot_commands}')
 
-        if (command == "hello"):
+        if command == "hello":
             author = str(message.author).split("#")
             await message.channel.send(f'Hello, {author[0]}')
-        
 
+        
 client.run(os.environ['TOKEN'])
