94293883b06eb97686984a1e8c38e14c173065ae
Ali Efe <65660565+A713F3@users.noreply.github.com>
2022-04-13 20:28:52 +0300

    added libraryhelp file

---
+++ libraryhelp.py
@@ -0,0 +1,35 @@
+language_list = ["C"]
+
+libraries_c = {
+"<assert.h>":"Contains the assert macro, used to assist with detecting logical errors and other types of bugs in debugging versions of a program.",
+"<complex.h>":"A set of functions for manipulating complex numbers.",
+"<ctype.h>":"Defines set of functions used to classify characters by their types or to convert between upper and lower case in a way that is independent of the used character set (typically ASCII or one of its extensions, although implementations utilizing EBCDIC are also known).",
+"<errno.h>":"For testing error codes reported by library functions.",
+"<fenv.h>":"Defines a set of functions for controlling floating-point environment.",
+"<float.h>":"Defines macro constants specifying the implementation-specific properties of the floating-point library.",
+"<inttypes.h>":"Defines exact-width integer types.",
+"<iso646.h>":"Defines several macros that implement alternative ways to express several standard tokens. For programming in ISO 646 variant character sets.",
+"<limits.h>":"Defines macro constants specifying the implementation-specific properties of the integer types.",
+"<locale.h>":"Defines localization functions.",
+"<math.h>":"Defines common mathematical functions.",
+"<setjmp.h>":"Declares the macros setjmp and longjmp, which are used for non-local exits.",
+"<signal.h>":"Defines signal-handling functions.",
+"<stdalign.h>":"For querying and specifying the alignment of objects.",
+"<stdarg.h>":"For accessing a varying number of arguments passed to functions.",
+"<stdatomic.h>":"For atomic operations on data shared between threads.",
+"<stdbool.h>":"Defines a boolean data type.",
+"<stddef.h>":"Defines several useful types and macros.",
+"<stdint.h>":"Defines exact-width integer types.",
+"<stdio.h>":"Defines core input and output functions",
+"<stdlib.h>":"Defines numeric conversion functions, pseudo-random numbers generation functions, memory allocation, process control functions",
+"<stdnoreturn.h>":"For specifying non-returning functions",
+"<string.h>":"Defines string-handling functions",
+"<tgmath.h>":"Defines type-generic mathematical functions.",
+"<threads.h>":"Defines functions for managing multiple threads, mutexes and condition variables",
+"<time.h>":"Defines date- and time-handling functions",
+"<uchar.h>":"Types and functions for manipulating Unicode characters",
+"<wchar.h>":"Defines wide-string-handling functions",
+"<wctype.h>":"Defines set of functions used to classify wide characters by their types or to convert between upper and lower case"
+}
+
+
