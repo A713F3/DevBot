d635b2eabf0a6d3b25c4a781b2dbc096077df2e9
Ali Efe <65660565+A713F3@users.noreply.github.com>
2022-04-13 20:29:11 +0300

    added info command

--- main.py
+++ main.py
@@ -1,20 +1,26 @@
 import discord
 import os
 from dotenv import load_dotenv
+from libraryhelp import *
 
 load_dotenv('TOKEN.env')
 
 client = discord.Client()
 
 bot_command = '&'
-bot_commands = ['help', 'hello']
+bot_commands = {
+    "help":" => Show commands.", 
+    "hello":" => Greets user", 
+    "info":" -language -library => Shows information about library."
+    }
+help_message = "\n".join([c + bot_commands[c] for c in bot_commands.keys()])
 
 def low(str):
     return str.lower()
 
 @client.event
 async def on_ready():
-    print('We have logged in as {0.user}'.format(client))
+    print("We have logged in as {0.user}".format(client))
 
 @client.event
 async def on_message(message):
@@ -28,20 +34,33 @@ async def on_message(message):
         command = low(parsed[0])
         arguments = list(map(low, parsed[1:]))
 
-        if command not in bot_commands:
+        if command not in bot_commands.keys():
             await message.channel.send(f"Invalid command, use {bot_command}help command for help.")
             return 
 
         print(f"\nAUTHOR: {message.author}")
-        print(f"COMMAND: {command} ")
+        print(f"COMMAND: {command}")
         print(f"ARGUMENTS: {arguments}")
         
         if command == "help":
-            await message.channel.send(f'Command List: {bot_commands}')
-
+            await message.channel.send(f"> Command List:\n```{help_message}```")
+            
         if command == "hello":
             author = str(message.author).split("#")
-            await message.channel.send(f'Hello, {author[0]}')
+            await message.channel.send(f"Hello, {author[0]}")
 
-        
-client.run(os.environ['TOKEN'])
+        if command == "info":
+            if len(arguments) < 2:
+                await message.channel.send(f"Language and library argument needed!")
+                return
+            
+            if arguments[0] == "-c":
+                await message.channel.send(f"> **{arguments[1]}:** \n```{libraries_c[arguments[1]]}```")
+            else:
+                await message.channel.send(f"**Language not supported**,\nSupported languages => {language_list}")
+            
+            
+
+
+
+client.run(os.environ["TOKEN"])
